c829cee658fdd6a8fe9625b112022abba5c61fcd
parkeunae <eapark108@gmail.com>
2021-01-04 22:56:50 +0900

    feat: add auth

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
+const { auth } = require('./middleware/auth');
 
 const { User } = require('./models/User');
 
@@ -76,6 +77,20 @@ app.post('/login', (req, res) => {
             })
         })
     })
+})
+
+app.get('/api/users/auth', auth, (req, res) => {
+    // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 true라는 말
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    });
 })
 
 
--- models/User.js
+++ models/User.js
@@ -86,6 +86,22 @@ userSchema.methods.generateToken = function(cb) {
     })
 }
 
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+
+    // 토큰을 decode 한다.
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        // 유저 아이디를 이용하여 유저를 찾은 다음
+        // 클라이언트에서 가져온 token과 DB에 보관된 token이 일치하는지 확인
+
+        user.findOne({"_id": jwt.decoded, "token": token}, function(err, user) {
+            if (err) return cb(err);
+
+            cb(null, user);
+        })
+    })
+}
+
 const User = mongoose.model('User', userSchema)
 
 // User model을 다른 곳에서도 쓸 수 있도록 export
