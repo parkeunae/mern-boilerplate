d62322a7a14cdfa714cb845a451964c755914624
parkeunae <eapark108@gmail.com>
2021-01-05 23:47:11 +0900

    feat: add logout function

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')
+            .then(response => {
+                if (response.data.success) {
+                    props.history.push('/login')
+                } else {
+                    alert('로그아웃 실패')
+                }
+            })
+    }
+
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh' }}>
             <h1>시작 페이지</h1>
+
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
         </div>
     );
 }
