4659b11a129cd9749b8128c5e59500b9ba0ba859
parkeunae <eapark108@gmail.com>
2021-01-05 18:20:35 +0900

    feat: add redux

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+// import user from './user_reducer';
+
+// 여러 Reducer들을 rootReducer로 합쳐주는 기능
+const rootReducer = combineReducers({
+    // user,
+})
+
+export default rootReducer;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { Provider } from 'react-redux';
 import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleWare from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
 
+// Object만 받는 Store가 Promise, Function도 받을 수 있도록 해주는 redux-promise, redux-thunk middleware 추가
+// createStore로 Redux에 Store 생성
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleWare, ReduxThunk)(createStore)
+
+// <Provider>로 Redux와 어플리케이션을 연결
+// Provider의 store에 Reducer를 넣어준다
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider
+    store={createStoreWithMiddleware(Reducer,
+      // Redux를 더 편하게 쓸 수 있도록 Redux Devtools extension 추가
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
