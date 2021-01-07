5da56376a6e5264e860fdf721ab4f98428e7f935
parkeunae <eapark108@gmail.com>
2021-01-07 23:52:12 +0900

    fix: rename auth hoc

--- client/src/App.js
+++ client/src/App.js
@@ -8,16 +8,16 @@ import {
 import LandingPage from './components/views/LandingPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
-import Auth from './hoc/auth';
+import withAuth from './hoc/withAuth';
 
 function App() {
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path="/" component={Auth(LandingPage)} />
-          <Route exact path="/login" component={Auth(LoginPage, false)} />
-          <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/" component={withAuth(LandingPage)} />
+          <Route exact path="/login" component={withAuth(LoginPage, false)} />
+          <Route exact path="/register" component={withAuth(RegisterPage, false)} />
         </Switch>
       </div>
     </Router>
