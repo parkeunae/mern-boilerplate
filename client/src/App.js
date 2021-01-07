1b25d40d9046039b4af9c4300c57666301fa0a3a
parkeunae <eapark108@gmail.com>
2021-01-07 22:44:22 +0900

    feat: add footer

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import NavBar from './components/views/NavBar/NavBar';
 import LandingPage from './components/views/LandingPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import Footer from './components/views/Footer/Footer';
 import Auth from './hoc/auth';
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
         </Switch>
       </div>
+      <Footer />
     </Router>
   )
 }
