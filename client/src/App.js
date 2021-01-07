80e56dec413f7f1e386be993910b89c0072e8d4e
parkeunae <eapark108@gmail.com>
2021-01-07 22:43:58 +0900

    feat: add navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
-
+import NavBar from './components/views/NavBar/NavBar';
 import LandingPage from './components/views/LandingPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
@@ -14,6 +14,7 @@ import Auth from './hoc/auth';
 function App() {
   return (
     <Router>
+      <NavBar />
       <div>
         <Switch>
           <Route exact path="/" component={Auth(LandingPage)} />
--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 function NavBar() {
     return (
-        <div>
-            NavBar
-        </div>
+        <nav>
+            <h1>Navigation Menu</h1>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="#">Menu1</a></li>
+                <li><a href="#">Menu2</a></li>
+            </ul>
+            <ul>
+                <li><a href="/login">Sign in</a></li>
+                <li><a href="/register">Sign up</a></li>
+            </ul>
+        </nav>
     );
 }
 
